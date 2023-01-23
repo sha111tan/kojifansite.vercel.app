4e2453e5946b2616b1d7706b537ec127890d0aac
kojifansite <okorokshinoda@gmail.com>
2023-01-23 20:24:11 +0700

    First commit
    First commit

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function linkAction(){
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-
+        
 /*===== ПЛАВНОЕ ОТКРЫТИЕ СТРАНИЦ =====*/
 setTimeout(function(){
 	document.body.classList.add('body_visible');
@@ -31,7 +31,7 @@ function scrollActive(){
     const scrollY = window.pageYOffset
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight,
-              sectionTop = current.offsetTop - 58,
+              sectionTop = current.offsetTop - 200,
               sectionId = current.getAttribute('id')
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
